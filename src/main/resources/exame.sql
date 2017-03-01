cd592a0d8ba04921862e2f8bf1a215ef9f7f290a
fidelisfelipe <atosfiel@gmail.com>
2017-03-01 09:37:57 -0300

    script dominio

---
+++ src/main/resources/exame.sql
@@ -0,0 +1,24 @@
+﻿INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (1,'Hemáceas','HEM', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (2,'Hemoglobina','HB', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (3,'Hematócrito','HCT', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (4,'Hemoglobina Corpuscular Média','HCM', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (5,'Volume Corpuscular Médio','VCM', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (6,'Concentração de Hemoglobina Corpuscular Média','CHCM', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (7,'Leucócitos','LEUC', 1);
+INSERT INTO public.exame(
+            id_exame, nome, sigla, id_tipoexame)
+    VALUES (8,'Segmentados','SEG', 1);
---
+++ src/main/resources/tipoexame.sql
@@ -0,0 +1,48 @@
+﻿INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (1,'Hemograma');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (2,'EAS');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (3,'Dosagens Hormonais');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (4,'Lipidogramas');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (5,'Parametros de Acompanhamento do HIV');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (6,'Parasitológico');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (7,'Bioquímica');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (8,'Provas de Função Hepática');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (9,'Exames Diretos e Culturas');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (10,'Especialidades');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (11,'Raio X');
+
+INSERT INTO public.tipoexame(
+            id_tipoexame, nome)
+    VALUES (12,'Eletrocardiogramas');
+
